e5af8846fdf41a28c85d1cd819651591c2363416
negifunyai <negifunyai@gmail.com>
2013-04-06 03:55:17 +0900

    RT通知に対応

--- osx_notify.rb
+++ osx_notify.rb
@@ -5,7 +5,14 @@ require 'terminal-notifier'
 Plugin.create(:osx_notify) do
   on_favorite do |service, user, message|
     if message.from_me?
-      TerminalNotifier.notify(message, :title => "@#{user[:idname]}", :subtitle => 'お気に入りに追加されました')
+      TerminalNotifier.notify(message, :title => "@#{user[:idname]}", :subtitle => 'ふぁぼふぁぼされました')
     end
+  end
+
+  on_retweet do |retweets|
+    retweets.each do |retweet|
+      if retweet.to_s and retweet.retweet_source.from_me?
+        TerminalNotifier.notify(retweet, :title => "@#{retweet.user[:idname]}", :subtitle => 'RTされました') end
+      end
   end
 end
